33c161916593f8a0841f96a3f6d5be2d5880b9cc
Elvis-785 <kiruielvis3358@gmail.com>
2024-11-28 19:54:54 +0300

    react state

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // Create a state variable to track if the item is in the cart
+  const [isInCart, setIsInCart] = useState(false);
+
+  // Function to handle the click event on the button
+  function handleAddToCart() {
+    setIsInCart((isInCart) => !isInCart);
+  }
+
+  // Set className based on isInCart state
+  const itemClass = isInCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
